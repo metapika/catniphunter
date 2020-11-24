35aee71dc9bffe6f18e0d0f3600b117a82b5ebb8
Meta <meta@pop-os.localdomain>
2020-11-24 09:40:32 +0100

    Nothing new, just saving

---
+++ Assets/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private int maxHealth = 100;
+    int currentHealth = 100;
+    void Start()
+    {
+        currentHealth = maxHealth;   
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        //play hurt
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Enemy Died!");
+        //Die animation
+
+        //Disable the enemy
+    }
+}
